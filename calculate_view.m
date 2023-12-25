d0dbe1e341087122543ce5eb10fd8821f29d55f3
Giorgio Cappello <133334515+GiorgioCappello@users.noreply.github.com>
2023-12-26 00:02:27 +0100

    Funzione per calcolare le view

---
+++ calculate_view.m
@@ -0,0 +1,10 @@
+function [PI_new, sigma_new, w_new] = calculate_view(sigma_SCC, PI, P, Q, Certainty)
+    % Calcolo di una view generica
+    % Calcolo di Omega
+    Omega = P * sigma_SCC * P' * ((1 - Certainty) / Certainty);
+    
+    % Calcolo dei nuovi parametri
+    PI_new = inv(inv(sigma_SCC) + P' * inv(Omega) * P) * (inv(sigma_SCC) * PI + P' * inv(Omega) * Q);
+    sigma_new = inv(inv(sigma_SCC) + P' * inv(Omega) * P);
+    w_new = (sigma_new \ PI_new) / sum(sigma_new \ PI_new);
+end
