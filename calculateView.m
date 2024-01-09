7303fdc5f56545c43717c61eae7d540b856d0ec0
Giorgio Cappello <133334515+GiorgioCappello@users.noreply.github.com>
2024-01-09 09:10:13 +0100

    update view unita

--- calculateView.m
+++ calculateView.m
@@ -1,7 +1,9 @@
 function [PI_new, sigma_new, w_new] = calculate_view(sigma_SCC, PI, P, Q, Certainty)
     % Calcolo di una view generica
     % Calcolo di Omega
-    Omega = P * sigma_SCC * P' * ((1 - Certainty) / Certainty);
+    for i=1:size(P,1)
+        Omega(i,i)=P(i,:)*sigma_SCC*P(i,:)'*((1-Certainty(i))*1/Certainty(i));
+    end
     
     % Calcolo dei nuovi parametri
     PI_new = inv(inv(sigma_SCC) + P' * inv(Omega) * P) * (inv(sigma_SCC) * PI + P' * inv(Omega) * Q);
